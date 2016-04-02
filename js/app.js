ccac6c859828479dcd6d1a2aad17d40930589ba5
Lee Hoyt <leehoyt@bellsouth.net>
2016-04-02 11:12:31 -0500

    Linked pic to multiple houses

--- js/app.js
+++ js/app.js
@@ -1,78 +1,96 @@
 var initialHouses = [
 		{
-			house2Src: 'img/house/house_green-min.jpg',
+			house1Src: 'img/house1/house_green-min.jpg',
+			house2Src: 'img/house2/House_Green-min.jpg',
 			buttonSrc: 'img/buttons/green.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_stone-min.jpg',
+			house1Src: 'img/house1/house_stone-min.jpg',
+			house2Src: 'img/house2/House_Stone-min.jpg',
 			buttonSrc: 'img/buttons/stone.png'
+
 		},
 
 		{
 
-			house2Src: 'img/house/house_brown-min.jpg',
+			house1Src: 'img/house1/house_brown-min.jpg',
+			house2Src: 'img/house2/House_Brown-min.jpg',
 			buttonSrc: 'img/buttons/brown.png'
+
 		},
 
 		{
 
-			house2Src: 'img/house/house_red-min.jpg',
+			house1Src: 'img/house1/house_red-min.jpg',
+			house2Src: 'img/house2/House_Red-min.jpg',
 			buttonSrc: 'img/buttons/red.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_charcoal-min.jpg',
+			house1Src: 'img/house1/house_charcoal-min.jpg',
+			house2Src: 'img/house2/House_Charcoal-min.jpg',
 			buttonSrc: 'img/buttons/charcoal.png'
+
 		},
 
 		{
 
-			house2Src: 'img/house/house_white-min.jpg',
+			house1Src: 'img/house1/house_white-min.jpg',
+			house2Src: 'img/house2/House_White-min.jpg',
 			buttonSrc: 'img/buttons/white.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_blue-min.jpg',
+			house1Src: 'img/house1/house_blue-min.jpg',
+			house2Src: 'img/house2/House_Blue-min.jpg',
 			buttonSrc: 'img/buttons/blue.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_beige-min.jpg',
+			house1Src: 'img/house1/house_beige-min.jpg',
+			house2Src: 'img/house2/House_Beige-min.jpg',
 			buttonSrc: 'img/buttons/beige.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_gray-min.jpg',
+			house1Src: 'img/house1/house_gray-min.jpg',
+			house2Src: 'img/house2/House_Gray-min.jpg',
 			buttonSrc: 'img/buttons/gray.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_slate-min.jpg',
+			house1Src: 'img/house1/house_slate-min.jpg',
+			house2Src: 'img/house2/House_Slate-min.jpg',
 			buttonSrc: 'img/buttons/slate.png'
 		},
 
 		{
 
-			house2Src: 'img/house/house_tan-min.jpg',
+			house1Src: 'img/house1/house_tan-min.jpg',
+			house2Src: 'img/house2/House_Tan-min.jpg',
 			buttonSrc: 'img/buttons/tan.png'
 		}
 	];
 
+
+
 var House = function(data) {
-
-	this.house1Src = ko.observable(data.house1Src);
-	this.house2Src = ko.observable(data.house2Src);
+	this.imgSrc = ko.observable('img/house1/house_green-min.jpg');
+	this.img1Src = ko.observable(data.house1Src);
+	this.img2Src = ko.observable(data.house2Src);
 	this.buttonSrc = ko.observable(data.buttonSrc);
-
-
 };
+
+var button1 = document.getElementById("setHouse1");
+var button2 = document.getElementById("setHouse2");
+
 
 var ViewModel = function() {
 	var self = this;
@@ -81,17 +99,28 @@ var ViewModel = function() {
 
 	initialHouses.forEach(function(houseItem) {
 		self.houseList.push( new House(houseItem));
+	console.log(houseItem);
 	});
 
 	this.currentHouse = ko.observable(this.houseList() [0] );
 
 	this.setHouse = function(clickedHouse) {
-		self.currentHouse(clickedHouse);
+
+		if (button1.checked){
+    		this.imgSrc = this.img1Src;
+    	}else {
+    		this.imgSrc = this.img2Src;
+    	};
+    	self.currentHouse(clickedHouse);
 	};
 
-	/*this.incrementCounter = function() {
-		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-	};*/
+	this.setHouse1 = function() {
+	document.getElementById("housepic").src = 'img/house1/house_green-min.jpg';
+	};
+
+	this.setHouse2 = function() {
+		document.getElementById("housepic").src = 'img/house2/House_Green-min.jpg';
+	};
 };
 
 
